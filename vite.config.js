7ac2c8a73ae638ed82b41bccf9654f149d463aac
Thiago <thiago.gui@hotmail.com>
2025-05-10 19:52:25 -0300

    Resolvendo conflitos para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  base: "/plantShopping/",
+  base: "/eplantShopping/",
   plugins: [react()],
 });
 // https://vitejs.dev/config/
