ac0c76c4b85dd96b90a3c22105ff5860f90dc1a6
Thiago <thiago.gui@hotmail.com>
2025-05-10 11:32:02 -0400

    corrigindo detalhes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './App';  // Your main component
-import store from './store';  // Path to your store configuration
-
-ReactDOM.render(
-  <Provider store={store}>
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import store from './store.js'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
-);
+    </Provider>
+  </React.StrictMode>,
+)
